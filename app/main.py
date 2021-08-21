846b7e0cc61d40d0fc9f6e15eabd90f24b02205d
ikeyu0806 <ikeyu0806@gmail.com>
2021-08-21 18:19:06 +0900

    add CORS setting

--- app/main.py
+++ app/main.py
@@ -1,14 +1,25 @@
 from typing import Optional
 
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
 
+origins = [
+    "http://localhost:5000",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 @app.get("/")
 def read_root():
     return {"Hello": "World"}
-
 
 @app.get("/items/{item_id}")
 def read_item(item_id: int, q: Optional[str] = None):
